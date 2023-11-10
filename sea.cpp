5537cd983bf6bf625ebab98d7b44ef810dda7153
Korobkov124 <144508844+Korobkov124@users.noreply.github.com>
2023-11-10 16:18:55 +0700

    Update1 sea.cpp

--- sea.cpp
+++ sea.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <clocale>
 #include <fstream>
 #include <vector>
@@ -10,11 +10,22 @@ using namespace std;
 int main()
 {
 	sea test;
-	vector<sea> vect;
+	string path = "txt.txt";
 	setlocale(LC_ALL, "ru");
-	ifstream txt("txt.txt");
-	if (txt.is_open()) {cout << "Файл открыт!\n";}
-	else {cout << "WARNING! Файл не открылся!!!\n";}
-	test.initialization(txt, test, vect);
+	ifstream txt;
+	txt.exceptions(ifstream::badbit | ifstream::failbit);
+	try
+	{
+		txt.open(path);
+	}
+	catch (const std::exception& ex)
+	{
+		cout << "Ошибка открытия файла!" << endl;
+		cout << ex.what() << endl;
+		return 0;
+	}
+	vector<sea> vect;
+	vect = test.initialization(txt, test);
+	test.output(vect, cout);
 	return 0;
 }
