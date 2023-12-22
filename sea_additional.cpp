b3cfa768d7e74c40bd7a6e2a40204d5cf1b79aff
Андрей Максимович Коробков <144508844+Korobkov124@users.noreply.github.com>
2023-12-23 00:42:08 +0700

    Update sea_additional.cpp

--- sea_additional.cpp
+++ sea_additional.cpp
@@ -1,3 +1,6 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
 #include "sea_additional.h"
 #include "fish_additional.h"
 #include <iostream>
@@ -10,8 +13,8 @@ using namespace std;
 vector<sea> sea::read_sea(istream& ist)
 {
     sea object;
-    vector <sea> vect; vector <fish> vect1;
-    while (!ist.eof())
+    vector <sea> vect;
+    while (!ist.eof() && !cin.fail())
     {
         object.name = object.read_name_of_sea(ist);
         ist >> object.depth >> object.salinity;
@@ -26,9 +29,8 @@ string sea::read_name_of_sea(istream& ist)
     string pod;
     int chet = 0;
     ist >> pod;
-    auto len = pod.length();
     string nos;
-    int i = 0;
+    size_t i = 0;
     while (chet != 2)
     {
         if (pod[i] == '\0')
@@ -50,18 +52,17 @@ string sea::read_name_of_sea(istream& ist)
             i++;
         }
     }
-    chet = 0;
     return nos;
 }
 
-void sea::write(vector<sea> vect, std::ostream& out)
+const void sea::write(const vector<sea> &vect, std::ostream& out)
 {
     sea object1;
-    for (int k = 0; k < vect.size(); k++)
+    for (size_t k = 0; k < vect.size(); k++)
     {
         object1 = vect[k];
         out << object1.name << " " << object1.depth << " " << object1.salinity << " ";
-        for (int i = 0; i < object1.name_of_fish.size(); i++)
+        for (size_t i = 0; i < object1.name_of_fish.size(); i++)
         {
             out << object1.name_of_fish[i].nameoffish << " ";
         }
